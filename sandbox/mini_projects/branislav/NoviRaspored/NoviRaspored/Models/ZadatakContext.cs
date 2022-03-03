11554a78de9ca33aa23cc337f8ff47ad90134b6e
branislav.radivojevic <wafflemynxyt@gmail.com>
2022-03-03 04:36:28 +0100

    testcommit1

---
+++ sandbox/mini_projects/branislav/NoviRaspored/NoviRaspored/Models/ZadatakContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NoviRaspored.Models
+{
+    public class ZadatakContext:DbContext
+    {
+        public ZadatakContext(DbContextOptions<ZadatakContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Zadatak> Rasporeds { get; set; }
+    }
+}
