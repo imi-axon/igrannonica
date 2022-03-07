306e78c7462bf9357ee69cc6ff5980f7b3880344
Nemanja <nemanjamilanovic757@gmail.com>
2022-03-07 23:26:41 +0100

    Kreiran kontroler za prihvatanje csv stringa koji ce se jos doradjivati

---
+++ src/back/BackApi/BackApi/Controllers/CSVstringController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CSVstringController : ControllerBase
+    {
+        private static List<CSVstring> tekstovi = new List<CSVstring>()
+        {
+            new CSVstring { csvtekst = "hello1" },
+            new CSVstring { csvtekst = "hello2" },
+            new CSVstring { csvtekst = "hello3" }
+        };
+        [HttpGet]
+        public async Task<ActionResult<List<CSVstring>>> Get()
+        {
+            return Ok(tekstovi);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<CSVstring>>> Post(CSVstring csvstring)
+        {
+            tekstovi.Add(csvstring);
+            return Ok(tekstovi);
+        }
+    }
+}
