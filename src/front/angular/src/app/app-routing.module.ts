67b5d26e6ea8f4a2deb7bb6b225a9e25ab4be3f9
Ivan Djordjevic <dj.iwan00@gmail.com>
2022-03-09 16:34:02 +0100

    Kreirana i testirana komponenta data-set-table za tabelarni prikaz skupa podataka. #20

--- src/front/angular/src/app/app-routing.module.ts
+++ src/front/angular/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CsvComponent } from './csv/csv.component';
+import { DataSetPageComponent } from './_components/_pages/data-set-page/data-set-page.component';
 import { ProjectPageComponent } from './_components/_pages/project-page/project-page.component';
 
 const routes: Routes = [
   {path:'csv',component:CsvComponent},
-  {path:'project',component:ProjectPageComponent}
+  {path:'project',component:ProjectPageComponent},
+  {path:'dataset',component:DataSetPageComponent}
 ];
 
 @NgModule({
@@ -15,4 +17,4 @@ const routes: Routes = [
 export class AppRoutingModule { }
 
 //Kada se dodaje nova putanja, dodati je u niz
-export const routingComponents=[CsvComponent, ProjectPageComponent];
+export const routingComponents=[CsvComponent, ProjectPageComponent, DataSetPageComponent];
