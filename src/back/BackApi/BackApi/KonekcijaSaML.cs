3780b03d7023b1cf884404f1c3b8bab4a4f75166
Nemanja <nemanjamilanovic757@gmail.com>
2022-03-12 23:46:12 +0100

    dodata klasa koja salje post request ML-u. Nije testirana. #12

---
+++ src/back/BackApi/BackApi/KonekcijaSaML.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.Diagnostics;
+
+namespace BackApi
+{
+    public static class KonekcijaSaML
+    {
+        public static async Task posaljihttp(string tekst)
+        {
+            Debug.WriteLine(tekst);
+            HttpClient client = new HttpClient();
+            StringContent content = new StringContent(tekst);
+
+            await client.PostAsync("http://localhost:5000", content);
+        }
+
+        /*public static async Task<dynamic> getRequest(string tekst)
+        {
+            Debug.WriteLine(tekst);
+            HttpClient client = new HttpClient();
+            StringContent content = new StringContent(tekst);
+
+            var result = await client.GetStringAsync("http://localhost:5000");
+            dynamic json = JsonConvert.DeserializeObject(result);
+            return json;
+        }*/
+    }
+}
