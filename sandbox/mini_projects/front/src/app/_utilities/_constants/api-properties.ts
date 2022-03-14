5c9e23a3d54a6603c6f5270268620d2e9572e1cd
Ivan Djordjevic <dj.iwan00@gmail.com>
2022-03-14 22:10:26 +0100

    Obrisan front sa sandbox-a

--- sandbox/mini_projects/front/src/app/_utilities/_constants/api-properties.ts
+++
@@ -1,4 +0,0 @@
-export const apiProperties = 
-{
-    url: 'http://localhost:7057'
-}
--- sandbox/mini_projects/front/src/app/_utilities/_constants/regExp.ts
+++
@@ -1,5 +0,0 @@
-export const regExp = 
-{
-    pattEmail: /^[a-zA-Z0-9]+([\.\-\+][a-zA-Z0-9]+)*\@([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}$/,
-    pattPass: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
-}
