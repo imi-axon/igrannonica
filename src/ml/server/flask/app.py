951a19abaf2df6b361512df4f5b602930d952c31
Dragor Draganovic <dragor.draganovic@gmail.com>
2022-03-15 23:36:02 +0100

    Add Data Set: Dodat kontroler za validaciju CSV-a

--- src/ml/server/flask/app.py
+++ src/ml/server/flask/app.py
@@ -1,16 +1,54 @@
+import csv
 import json
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def send_csv():
-    try:
-        # data = '{"A":"1","B":"2","C":"3"}'
-        data = request.data.decode()
-        converted = json.JSONDecoder().decode(data)
-        print(converted)
-    except:
-        print('Conversion Error')
+# Utilities
 
-    return '---'
+# Vraca informacije o formatu CSV stringa
+def get_csv_dialect(csv_str: str) -> csv.Dialect:
+
+    sniffer = csv.Sniffer()
+    dialect = sniffer.sniff(csv_str)
+    return dialect
+
+
+# Konvertuje JSON string u objekat
+def json_to_object(json_str: str) -> object | list:
+
+    return json.JSONDecoder().decode(json_str)
+
+
+# Konvertuje CSV string u listu listi (uz detekciju formata CSV-a)
+def csv_to_list(csv_str: str) -> list[list]:
+    
+    dialect = get_csv_dialect(csv_str)
+    lines = csv_str.splitlines()
+    reader = csv.reader(lines, dialect) # !!! Ima problem kad se koriste navodnici, tada iako redovi imaju razlicit broj elemenata konvertuje u niz
+    result = [x for x in reader]
+    print(result)
+    return result
+
+
+# True ako je konverzija uspesna, False u suprotnom
+def csv_is_valid(csv_str):
+
+    try:
+        csv_to_list(csv_str)
+        return True
+    except:
+        return False
+
+
+# Routes
+
+@app.route('/api/dataset/validate/csv', methods=['POST'])
+def validate_csv():
+
+    csvstring = request.data.decode()
+
+    if csv_is_valid(csvstring):
+        return ('CSV je u ispravnom formatu', 201)
+
+    return ('CSV nije u ispravnom formatu', 400)
