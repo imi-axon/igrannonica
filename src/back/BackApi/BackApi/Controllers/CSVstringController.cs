351b234fd2dca0bceccbb4aad519c77eafc12fe1
Nemanja <nemanjamilanovic757@gmail.com>
2022-03-15 22:17:18 +0100

    formirana osnova Get Data Set-a. Potrebno povezati sa bazom. API testiran preko postman-a. Potrebno testirati sa ML delom. #42

--- src/back/BackApi/BackApi/Controllers/CSVstringController.cs
+++ src/back/BackApi/BackApi/Controllers/CSVstringController.cs
@@ -4,25 +4,32 @@ using System.Diagnostics;
 
 namespace BackApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/dataset")]
     [ApiController]
     public class CSVstringController : ControllerBase
     {
         private string tekst;
-        [HttpGet]
-        public async Task<ActionResult<string>> Get(string csvstring)
+        [HttpGet("{project_id}/json")]
+        public async Task<ActionResult<dynamic>> Get(int project_id)
         {
-            tekst = csvstring;
-            return tekst;
+            //Debug.WriteLine(project_id);
+            //VADI SE IZ BAZE CSV STRING KOJI ODGOVARA DATOM ID-U
+            //UKOLIKO NEMA PROJKETA SA DATIM ID-EM VRACA VRACA NOT FOUND
+            //UKOLIKO KORISNIK NIJE ULOGOVAN VRACA UNAUTHORIZED
+
+            tekst = "n1;n2;n3;out\r1; 1; 0; 1\r1; 0; 0; 1\r0; 0; 1; 1\r1; 0; 1; 1\r0; 0; 0; 0\r";
+            return Task.Run((Func<Task>)(() => KonekcijaSaML.posaljihttp(tekst)));
         }
-        [HttpPost]
+        [HttpPost("post")]
         public async Task<ActionResult<string>> Post( [FromBody] CSVstring content)
         {
             string csvstring;
             csvstring = content.csvstring;
-            tekst = CsvValidacija.Validate(csvstring);
-            Task.Run((Func<Task>)(() => KonekcijaSaML.posaljihttp(tekst)));
+            Debug.WriteLine(csvstring);
+            tekst = CsvValidacija.Validate(csvstring);  //NE KORISTIMO VISE
+            //Task.Run((Func<Task>)(() => KonekcijaSaML.posaljihttp(tekst)));
             //KonekcijaSaML.posaljihttp(tekst);
+            //TREBA DA 
             return tekst;
         }
     }
