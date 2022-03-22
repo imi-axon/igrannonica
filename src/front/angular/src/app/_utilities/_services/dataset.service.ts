22575986cef17adb69218d57db1850208052a1df
Ivan Djordjevic <dj.iwan00@gmail.com>
2022-03-22 19:51:59 +0100

    Uradjen GET API za statistiku. #73

--- src/front/angular/src/app/_utilities/_services/dataset.service.ts
+++ src/front/angular/src/app/_utilities/_services/dataset.service.ts
@@ -49,11 +49,33 @@ export class DatasetService {
         if(response.status == HttpStatusCode.NotFound)
           if(self && notFoundCallback)
             notFoundCallback(self, response.body.message);
-        
-        
-        
       }
     );
   }
+  
+  GetStatistics(project_id: number, main: boolean, self?: any, successCallback?: Function, unauthorizedCallback?: Function, forbiddenCallback?: Function, notFoundCallback?: Function){
+    
+    this.datasetAPI.GetStatistics(project_id, main).subscribe(
+      (response) => {
+        
+        if(response.status == HttpStatusCode.Ok)
+          if(self && successCallback) 
+            successCallback(self, response.body);
+        
+        if(response.status == HttpStatusCode.Unauthorized)
+          if(self && unauthorizedCallback)
+            unauthorizedCallback(self, response.body.message);
+        
+        if(response.status == HttpStatusCode.Forbidden)
+          if(self && forbiddenCallback)
+            forbiddenCallback(self, response.body.message);
+        
+        if(response.status == HttpStatusCode.NotFound)
+          if(self && notFoundCallback)
+            notFoundCallback(self, response.body.message);
+      }
+    )
+  }
+  
   
 }
