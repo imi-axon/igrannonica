053ed7a798737a887634eb983c3e443970f5eb04
Dragor Draganovic <dragor.draganovic@gmail.com>
2022-03-24 00:54:06 +0100

    Kreiran model Statistics #75

--- src/ml/app/models.py
+++ src/ml/app/models.py
@@ -4,6 +4,9 @@ from pydantic import BaseModel
 class Dataset (BaseModel):
     dataset: str
 
+class Statistics (BaseModel):
+    statistics: str
+
 class DatasetEditActions (BaseModel):
     actions: str
     data: str
