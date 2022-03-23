f35b04d683e1a4233a0b86ab755e617fa324c44c
Dragor Draganovic <dragor.draganovic@gmail.com>
2022-03-23 01:14:25 +0100

    Istestiran convert_csv_to_json endpoint #74

--- src/ml/app/main.py
+++ src/ml/app/main.py
@@ -33,25 +33,29 @@ def validate_csv(body: Dataset, response: Response):
 
 
 # Get Dataset
-@app.post('/api/dataset/convert/json', status_code=201, response_model=Dataset | None)
+@app.post('/api/dataset/convert/json', status_code=201, response_model=Dataset)
 def convert_csv_to_json(body: Dataset, response: Response):
 
     print('Pocetak kontrolera (za Get Dataset)')
 
     csvstring = body.dataset
     
-    # print(csvstring)
-    resp: Dataset | None = None
+    print(csvstring)
+    resp = ''
 
     try:
         obj = csv_decode(csvstring)
-        # print(obj)
+        print(obj)
         resp = json_encode(obj)
-        # print(resp)
+        print(resp)
     except:
         response.status_code = status.HTTP_400_BAD_REQUEST
     
-    return resp
+    fin = {'dataset': resp}
+
+    print(fin)
+
+    return fin
 
 
 # Aktivnost: Edit Dataset
