39bbffefd1a6da6f8c6bab5c7a21bab0d4f80d36
Natalija Grujovic <natalija.grujovic.31@gmail.com>
2022-03-26 17:28:12 +0100

    Doradjena profilna strana

--- src/front/angular/src/app/_components/_elements/profil/profil.component.ts
+++ src/front/angular/src/app/_components/_elements/profil/profil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/_utilities/_services/auth.service';
 
 @Component({
   selector: 'app-profil',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth:AuthService) { }
 
   ngOnInit(): void {
   }
