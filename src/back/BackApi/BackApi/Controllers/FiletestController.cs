16816062a52a9f05c9ac0e3790308f43d85a8f32
Dragor Draganovic <dragor.draganovic@gmail.com>
2022-04-04 18:09:06 +0200

    Ispravljen Compile Error, dodat parametar u poziv ProjIdToPath metodu (main = false)

--- src/back/BackApi/BackApi/Controllers/FiletestController.cs
+++ src/back/BackApi/BackApi/Controllers/FiletestController.cs
@@ -29,7 +29,7 @@ namespace BackApi.Controllers
             if (userid == -1) return Unauthorized();
             if (!projsrv.projectOwnership(userid, projid)) return Forbid();
 
-            var path = storsrv.GetDataset(datasrv.ProjIdToPath(projid));
+            var path = storsrv.GetDataset(datasrv.ProjIdToPath(projid, false));
             var bytes = await System.IO.File.ReadAllBytesAsync(path);
 
             return File(bytes, "text/csv",Path.GetFileName(path));
@@ -38,7 +38,7 @@ namespace BackApi.Controllers
         [HttpGet("Storage/proj{pid}/data/data{did}.csv"),Host("localhost:7057","localhost:8000")] //adresa ml mikroserivsa
         public async Task<ActionResult> PassDatasetToML(int pid,int did)
         {
-            var path = storsrv.GetDataset(datasrv.ProjIdToPath(pid));
+            var path = storsrv.GetDataset(datasrv.ProjIdToPath(pid, false));
             var bytes = await System.IO.File.ReadAllBytesAsync(path);
 
             return File(bytes, "text/csv", Path.GetFileName(path));
