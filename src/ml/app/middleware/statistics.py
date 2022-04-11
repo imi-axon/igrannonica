c9f934a88b7fd0e486719dabe0fc9f27d945a075
Milica Vucic <milicavucic19@gmail.com>
2022-04-11 22:13:14 +0200

    Pomocne funkcije iz statistics.py prebacene u util.py

--- src/ml/app/middleware/statistics.py
+++ src/ml/app/middleware/statistics.py
@@ -108,24 +108,8 @@ from services.util import object_to_json
 
 import json
 import numpy as np
-
-class dictionary(dict):
-    def __init__(self):
-        self = dict()
-    
-    def add(self, key, value):
-        self[key] = value
-
-#dodato zbog: TypeError: Object of type int64 is not JSON serializable
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        if isinstance(obj, np.floating):
-            return float(obj)
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return super(NpEncoder, self).default(obj)
+from util import dictionary
+from util import NpEncoder
 
 class StatisticsMiddleware:
 
