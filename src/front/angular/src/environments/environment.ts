6dd5c769442c493244708b9dad78e0cc7b7e5eb9
Dragor Draganovic <dragor.draganovic@gmail.com>
2022-04-17 22:39:12 +0200

    Automatizovana promena api linkova pomocu environment.ts

--- src/front/angular/src/environments/environment.ts
+++ src/front/angular/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiProperties:
+  {
+    url: 'https://localhost:7057',
+    wsurl:'wss://localhost:7057'
+  }
 };
 
 /*
