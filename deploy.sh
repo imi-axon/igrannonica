3bd9f21eb7e70a809061cda334f6fc136d4327c6
Dragor Draganovic <dragor.draganovic@gmail.com>
2022-04-20 02:48:36 +0200

    izmenjen deploy, dodat server.js

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+scp -r src/back/BackApi/BackApi/bin/MyRelease Axon@softeng.pmf.kg.ac.rs:/home/Axon/prod/back/app
+scp -r src/front/angular/dist/angular Axon@softeng.pmf.kg.ac.rs:/home/Axon/prod/front/app
+scp -r src/ml/app Axon@softeng.pmf.kg.ac.rs:/home/Axon/prod/ml/app
+
+scp sandbox/server.js Axon@softeng.pmf.kg.ac.rs:/home/Axon/prod/front
--- src/deploy.sh
+++
@@ -1,3 +0,0 @@
-scp -r back/BackApi/BackApi/bin/MyRelease Axon@softeng.pmf.kg.ac.rs:/home/Axon/prod/back/app
-scp -r front/angular/dist/angular Axon@softeng.pmf.kg.ac.rs:/home/Axon/prod/front/app
-scp -r ml/app Axon@softeng.pmf.kg.ac.rs:/home/Axon/prod/ml/app
