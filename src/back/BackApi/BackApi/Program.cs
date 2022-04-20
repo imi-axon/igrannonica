9cb3987200e0574794b6fd3b46aa333b9bb016b8
Dragor Draganovic <dragor.draganovic@gmail.com>
2022-04-20 04:12:11 +0200

    Promenjen CORS

--- src/back/BackApi/BackApi/Program.cs
+++ src/back/BackApi/BackApi/Program.cs
@@ -92,8 +92,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: myAllowSpecificOrigins,
         builder =>
         {
-            builder.WithOrigins("http://localhost:4200")
-            //builder.AllowAnyOrigin()
+            //builder.WithOrigins("http://localhost:4200")
+            builder.AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader();
         });
