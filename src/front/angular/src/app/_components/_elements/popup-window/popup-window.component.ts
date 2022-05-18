f08e2cf1d4f9cbe3423e41722159ddd90c451e37
Natalija Grujovic <natalija.grujovic.31@gmail.com>
2022-05-18 17:28:51 +0200

    Jwt expired- logout

--- src/front/angular/src/app/_components/_elements/popup-window/popup-window.component.ts
+++ src/front/angular/src/app/_components/_elements/popup-window/popup-window.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from 'src/app/_utilities/_services/auth.service';
 
 @Component({
   selector: 'app-popup-window',
@@ -11,7 +12,8 @@ export class PopupWindowComponent implements OnInit {
 
   poruka:String="";
 
-  constructor(private translate:TranslateService, @Inject(MAT_DIALOG_DATA) public data:any, private dialog:MatDialogRef<PopupWindowComponent>) {
+  constructor(private translate:TranslateService, @Inject(MAT_DIALOG_DATA) public data:any, private dialog:MatDialogRef<PopupWindowComponent>,
+  public auth: AuthService) {
     dialog.disableClose=true;
     this.poruka=data.poruka;
    }
--- src/front/angular/src/app/_utilities/_services/auth.service.ts
+++ src/front/angular/src/app/_utilities/_services/auth.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { RedirectRoutes } from '../_constants/routing.properties';
 import { JWTUtil } from '../_helpers/jwt-util';
+import { PopupWindowComponent } from 'src/app/_components/_elements/popup-window/popup-window.component';
+import { MatDialog } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
+import { HttpClient, HttpResponse, HttpStatusCode } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { apiProperties } from '../_constants/api-properties';
+import { HeaderUtil } from '../_helpers/http-util';
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +19,21 @@ export class AuthService {
   public logovan:boolean;
   public korisnickoIme:string='';
   public ime_prezime: string='';
+  public exp: number;
 
-  constructor( public router: Router) { 
+  constructor( public router: Router, private dialog:MatDialog,private translate:TranslateService,private http: HttpClient) { 
     if(this.jwt=='') 
     {
       this.logovan=false;
       this.korisnickoIme='';
       this.ime_prezime='';
+      this.exp=0;
     }
     else {
        this.logovan=true;
        this.korisnickoIme=JWTUtil.getUsername();
        this.ime_prezime=JWTUtil.getFullName();
+       this.exp = JWTUtil.getExpDate();
     }
   }
 
@@ -34,5 +44,39 @@ export class AuthService {
     this.logovan=false;
     this.korisnickoIme='';
     this.ime_prezime='';
+  }
+
+  porukaPopup: String = "";
+  openDialog(){
+    //MORA OVAKO JER TRANLATE NE RADI 
+    if(localStorage.getItem('lang1')=='en')
+      this.porukaPopup = "Your session has expired. Please login again!";
+    else if(localStorage.getItem('lang1')=='sr')
+      this.porukaPopup = "Vaša sesija je istekla. Ulogujte se ponovo!";
+    let dialogRef = this.dialog.open(PopupWindowComponent, { data: { poruka: this.porukaPopup } });
+  }
+
+  checkJWTexpired(){
+      const exp = JWTUtil.getExpDate();
+      return (Math.floor((new Date).getTime() / 1000)) >= exp;
+  }
+
+  private expDate:number
+  private now:number
+
+  autoLogout(){
+    this.now=((new Date).getTime())/1000;
+    this.expDate=JWTUtil.getExpDate();
+    console.log(this.expDate);
+    console.log(this.now);
+    console.log(this.expDate-this.now);
+
+    setTimeout(()=>{this.tokenExpLogout()},(this.expDate-this.now)*1000);
+  }
+
+  tokenExpLogout(){
+    this.openDialog();
+    this.logout();
+
   }
 }
