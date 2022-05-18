ccc80562c0c024332fb1e50fe205270523ac0ce8
Milica Vucic <milicavucic19@gmail.com>
2022-05-18 17:55:26 +0200

    Prilagodjena funkcija

--- src/ml/app/services/metadata.py
+++ src/ml/app/services/metadata.py
@@ -12,7 +12,11 @@ class MetadataService :
         categorical_columns = dataframe.select_dtypes(include=['object']).columns.tolist()
         
         for column in numeric_columns:
-            kolonaDict = {"type": "num", "trainReady" : False, "encoding" : "null"}
+            num_nulls = dataframe[column].isna().sum()
+            if(num_nulls==0):
+                kolonaDict = {"type": "num", "trainReady" : True, "encoding" : "null"}
+            else:
+                kolonaDict = {"type": "num", "trainReady" : False, "encoding" : "null"}
             metadata["columns"].update({column : kolonaDict})
 
         for column in categorical_columns:
@@ -22,14 +26,14 @@ class MetadataService :
         return metadata
 
     
-    def updateTrainReady(self, metadata, dataframe, stats):
-
-        numeric_columns = dataframe.select_dtypes(exclude=['object']).columns.tolist()
-        for column in numeric_columns:
-            num_nulls = dataframe[column].isna().sum()
-            if(num_nulls == 0):
-                if(metadata['columns'][column]['type']=='num'):
-                    metadata['columns'][column]['trainReady'] = True
+    def updateTrainReady(self, metadata, stats):
+        columns = list(metadata['columns'])
+        for column in columns:
+            if(metadata['columns'][column]['type']=='num'):
+                for i in range(len(stats["colnulls"]['cols'])):
+                    if(column == stats["colnulls"]['cols'][i]):
+                        if(stats["colnulls"]["nulls"][i]==0):
+                            metadata['columns'][column]['trainReady'] = True
         #nema provere za kategorijske -> po default-u : False;  Pri enkodiranju se postavlja na True (tada sigurno nemaju null vrednosti i enkodirane su)
         metadata.update({"statistics" : stats})
         return metadata
