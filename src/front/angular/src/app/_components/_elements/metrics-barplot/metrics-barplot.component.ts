cc0aa96cfadb5019bf98830479ae8c619bd908af
Ivan Djordjevic <dj.iwan00@gmail.com>
2022-05-20 04:10:14 +0200

    Sredjen prikaz barplotova za metriku.

--- src/front/angular/src/app/_components/_elements/metrics-barplot/metrics-barplot.component.ts
+++ src/front/angular/src/app/_components/_elements/metrics-barplot/metrics-barplot.component.ts
@@ -15,6 +15,7 @@ export class MetricsBarplotComponent implements OnInit {
   @ViewChild("plot3")
   plot3: ElementRef;
   
+  public max: number = 0;
   
   public value1: number = 0;
   public value2: number = 0;
@@ -29,8 +30,13 @@ export class MetricsBarplotComponent implements OnInit {
     this.value1 = v1;
     this.value2 = v2;
     
-    this.plot1.nativeElement.setAttribute('style', 'width: ' + (this.value1 * 100) + '%');
-    this.plot2.nativeElement.setAttribute('style', 'width: ' + (this.value2 * 100) + '%');
+    if(this.value1 > this.max)
+      this.max = this.value1;
+    if(this.value2 > this.max)
+      this.max = this.value2;
+    
+    this.plot1.nativeElement.setAttribute('style', 'width: ' + (this.value1 / this.max * 100) + '%');
+    this.plot2.nativeElement.setAttribute('style', 'width: ' + (this.value2 / this.max * 100) + '%');
   }
   
   
