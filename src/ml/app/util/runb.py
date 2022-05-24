e183ae488f4740aa06fde4918ad460f077fb0242
Dragor Draganovic <dragor.draganovic@gmail.com>
2022-05-24 09:12:14 +0200

    Kreirane wraper funkcije za Lock acquire i release #197

--- src/ml/app/util/runb.py
+++ src/ml/app/util/runb.py
@@ -1,6 +1,19 @@
 import asyncio
+from threading import Lock
 
 # Run Blocking (run b)
 # Wraper oko poziva blokirajuce funkcije koji ne blokira izvrsavanje ostalih funkcija (ne blokira eventloop)
 async def runb(fun, *args):
     return await asyncio.get_event_loop().run_in_executor(None, fun, *args)
+
+
+
+# -- Wraperi za specificne funkcije (koristeci runb wraper) --
+
+# Asinhroni Lock
+async def alock(lock: Lock):
+    return await runb(lock.acquire)
+
+# Asinhroni Unlock
+async def aunlock(lock: Lock):
+    return await runb(lock.release)
