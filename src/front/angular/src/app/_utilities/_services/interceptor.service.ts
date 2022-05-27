baffa8543872e877e89a73a26723e6a45e863171
Natalija Grujovic <natalija.grujovic.31@gmail.com>
2022-05-27 12:11:55 +0200

    Trajanje loadera

--- src/front/angular/src/app/_utilities/_services/interceptor.service.ts
+++ src/front/angular/src/app/_utilities/_services/interceptor.service.ts
@@ -34,13 +34,13 @@ export class InterceptorService implements HttpInterceptor {
 
     this.totalRequests++;
    // console.log("req" + this.totalRequests);
-    setTimeout(() => { this.loaderService.isLoading.next(true); }, 0)
+    setTimeout(() => { this.loaderService.isLoading.next(true); },0)
     return next.handle(req).pipe(
       finalize(() => {
      //   console.log("obradioreq" + this.totalRequests);
         this.totalRequests--;
         if (this.totalRequests == 0){
-          setTimeout(() => { this.loaderService.isLoading.next(false); }, 0)
+          setTimeout(() => { this.loaderService.isLoading.next(false); },680)
        //   console.log("Obradio poslednji req");
         }
       }
