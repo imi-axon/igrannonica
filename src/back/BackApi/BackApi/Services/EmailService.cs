b41f9123299da0d35d836f4c90f81ade3369f032
Nemanja <nemanjamilanovic757@gmail.com>
2022-06-04 21:25:35 +0200

    Izmenjen izgled emaila. Potrebno je jos doraditi.

--- src/back/BackApi/BackApi/Services/EmailService.cs
+++ src/back/BackApi/BackApi/Services/EmailService.cs
@@ -70,13 +70,32 @@ namespace BackApi.Services
             message.To.Add(MailboxAddress.Parse(receiver));
             message.Subject = title;
 
+            var storage = @"Storage";
+            var photopath = Path.Combine("Storage", "profilna.png");
             var bodybuilder = new BodyBuilder();
             if (ind == 1)
-                bodybuilder.HtmlBody = string.Format(@"<p>Kliknite na dugme kako biste se verifikovali</p>
-                <a href='" + textmessage + "'><input type='button' style='background:blue; color:red' value='VERIFIKUJ SE'></a>");
+                bodybuilder.HtmlBody = string.Format(@"<div style='background-color:#0080DD; text-align:center; margin: 20px;'><h1 style='color:white;'>Dobro došli</h1>" +
+                "<img src=''>" +
+                "<h3 style='color: white'>Potrebno je da klikom na dugme ispod potvrdite registracuju</h3>"+
+                "<a href='"+ textmessage + "'><input type = 'button' value = 'Potvrdi' style='background-color: white; color: #003FB9; padding: 16px 50px; font-size:20px;'></a><br><br>" +
+                "<p style='color:white;'> Ukoliko dugme ne radi, kopirjte sledeći link u vaš pretraživač</p>"+
+                "<a style='color:white;' href='" + textmessage+ "'> "+ textmessage + "</a>"+
+                "<br>"+
+                "<h3 style='color:white;'> Ukoliko imate dodatnih pitanja, odgovorite na ovaj email, mi smo uvek tu za vas da vam pomognemo.</h3" +
+
+                "<hp style='color:white;'> Pozdrav,<br> Tim AXON </p></div>"
+                );
             else
-                bodybuilder.HtmlBody = string.Format(@"<p>Kliknite na dugme kako biste promenili email</p>
-                <a href='" + textmessage + "'><input type='button' style='background:blue; color:red' value='POTVRDI'></a>");
+                bodybuilder.HtmlBody = string.Format(@"<div style='background-color:#0080DD; text-align:center; margin: 20px;'><img src=''>" +
+                "<h3 style='color: white'>Potrebno je da klikom na dugme ispod potvrdite promenu email-a</h3>" +
+                "<a href='" + textmessage + "'><input type = 'button' value = 'Potvrdi' style='background-color: white; color: #003FB9; padding: 16px 50px; font-size:20px;'></a><br><br>" +
+                "<p style='color:white;'> Ukoliko dugme ne radi, kopirjte sledeći link u vaš pretraživač</p>" +
+                "<a style='color:white;' href='" + textmessage + "'> " + textmessage + "</a>" +
+                "<br>" +
+                "<h3 style='color:white;'> Ukoliko imate dodatnih pitanja, odgovorite na ovaj email, mi smo uvek tu za vas da vam pomognemo.</h3" +
+
+                "<p style='color:white;'> Pozdrav,<br> Tim AXON </p></div>"
+                );
 
             message.Body=bodybuilder.ToMessageBody();
             SmtpClient client = new SmtpClient();
@@ -109,8 +128,16 @@ namespace BackApi.Services
             message.Subject = title;
 
             var bodybuilder = new BodyBuilder();
-            bodybuilder.HtmlBody = string.Format(@"<p>Kliknite na dugme kako biste potvrdili promenu lozinke</p>
-                <a href='" + textmessage + "'><input type='button' style='background:blue; color:red' value='PROMENI LOZINKU'></a>");
+            bodybuilder.HtmlBody = string.Format(@"<div style='background-color:#0080DD; text-align:center; margin: 20px;'><img src=''>" +
+                "<h3 style='color: white'>Potrebno je da klikom na dugme ispod potvrdite promenu šifre</h3>" +
+                "<a href='" + textmessage + "'><input type = 'button' value = 'Potvrdi' style='background-color: white; color: #003FB9; padding: 16px 50px; font-size:20px;'></a><br><br>" +
+                "<p style='color:white;'> Ukoliko dugme ne radi, kopirjte sledeći link u vaš pretraživač</p>" +
+                "<a style='color:white;' href='" + textmessage + "'> " + textmessage + "</a>" +
+                "<br>" +
+                "<h3 style='color:white;'> Ukoliko imate dodatnih pitanja, odgovorite na ovaj email, mi smo uvek tu za vas da vam pomognemo.</h3" +
+
+                "<p style='color:white;'> Pozdrav,<br> Tim AXON </p></div>"
+                );
 
             message.Body = bodybuilder.ToMessageBody();
             SmtpClient client = new SmtpClient();
